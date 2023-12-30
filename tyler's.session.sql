dbf4090b53422e7e8e5dc802532464acc0f9a4f0
TylerRamil <tylerjayramil@gmail.com>
2023-12-30 09:47:44 -0800

    Queries

--- tyler's.session.sql
+++ tyler's.session.sql
@@ -1,2 +1,14 @@
-SELECT SUM(total_price) / COUNT(DISTINCT order_id)
-from pizza_sales
+SELECT *
+FROM pizza_sales
+SELECT SUM(total_price) AS Total_Revenue
+FROM pizza_sales;
+SELECT (SUM(total_price) / COUNT(DISTINCT order_id)) AS Avg_Order_Value
+FROM pizza_sales
+SELECT SUM(quantity) AS Total_Pizza_Sold
+FROM pizza_sales
+SELECT COUNT(DISTINCT order_id) AS Total_Orders
+FROM pizza_sales
+SELECT CAST(
+        CAST(SUM(quantity) AS DECIMAL(10, 2)) / CAST(COUNT(DISTINCT order_id) AS DECIMAL(10, 2)) AS DECIMAL(10, 2)
+    ) AS Avg_Pizza_Per_Order
+FROM pizza_sales
